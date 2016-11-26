10f60cda49d49441055e1676ca3df56e66e13e07
kodenmouss <yoannbodier@hotmail.com>
2016-11-26 23:08:39 +0100

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="title" content="Blog">
+  <meta name="keywords" content="blog">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Blog</h1>
+  <?php
+    include("menu.php");
+  ?>
+</body>
+</html>
---
+++ video.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="title" content="Videos">
+  <meta name="keywords" content="media">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Vidéo</h1>
+  <?php
+    include("menu.php");
+  ?>
+</body>
+</html>
