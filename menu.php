39cdf12e64b0d0aefc4138c0657edc6883e413f4
kodenmouss <yoannbodier@hotmail.com>
2016-11-28 20:25:29 +0100

    Add files via upload

--- menu.php
+++ menu.php
@@ -1,181 +1,34 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta content="text/html; charset=windows-1252" http-equiv="content-type">
-    <title>menu</title>
-  </head>
-  <body>
-    <p style="width: 120px;"><a title="menu" href="prealpha2.html">menu</a></p>
-    <p style="width: 120px;"><span style="font-style: italic;"><span style="text-decoration: underline;"><span
-            style="font-weight: bold;">Projet</span></span></span></p>
-    <p style="width: 120px;"><a href="projet%20musicklock.php">musiclock</a></p>
-    <p style="width: 120px;"><a href="muz.php">muziclock</a></p>
-    <p style="width: 120px;"><a href="progavr.php">proger avr</a></p>
-    <p style="width: 120px;"><span style="font-style: italic;"><span style="text-decoration: underline;"><span
-            style="font-weight: bold;"><br>
-          </span></span></span></p>
-    <p style="width: 120px;"><span style="font-style: italic;"><span style="text-decoration: underline;"><span
-            style="font-weight: bold;"><br>
-          </span></span></span></p>
-    <p style="width: 120px;"><span style="font-style: italic;"><span style="text-decoration: underline;"><span
-            style="font-weight: bold;">NAS</span></span></span></p>
-    <p style="width: 120px;"><a title="other" href="nas%20dns%20320%20presentation.php">nas
-        dns320<br>
-      </a></p>
-    <p style="width: 120px;">ffp 0.5</p>
-    <p style="width: 120px;"><a href="nas%20dns320%20configuration.php">nas conf</a></p>
-    <p style="width: 120px;"><a href="nas%20dns%20320%20changer%20ip%20du%20webui%20dans%20squeeze.php">nas
-        changer port du webui</a></p>
-    <p style="width: 120px;"><a href="nas%20ffp%200.7.php">nas ffp 0.7</a></p>
-    <p style="width: 120px;"><a href="nas%20ffp%20debian.php">nas debian</a></p>
-    <p style="width: 120px;"><a href="nas%20ffp%20debian%20alternative.php">nas
-        debian 2</a></p>
-    <p style="width: 120px;"><a href="nas%20install%20debian%203.php">nas debian
-        3</a></p>
-    <p style="width: 120px;"><a href="nas%20lighttpd%20debian.php">nas lighttpd</a></p>
-    <p style="width: 120px;"><a href="nas%20mediatomb%20debian.php">nas deb
-        mediatomb</a></p>
-    <p style="width: 120px;"><a href="nas%20ffpdebian%20sur%20usb.php">nas
-        ffp/deb sur usb</a></p>
-    <p style="width: 120px;"><br>
-    </p>
-    <p style="width: 120px;"><span style="text-decoration: underline;"><span style="font-style: italic;"><span
-            style="font-weight: bold;">FF7</span></span></span></p>
-    <p style="width: 120px;"><a href="FF7%20genese%20et%20fontionnemt.php">ff7
-        genese</a></p>
-    <p style="width: 120px;"><a href="FF7%20moteur.php">ff7 moteur</a></p>
-    <p style="width: 120px;"><a href="FF7%20installation%20et%20config.php">ff7
-        install</a></p>
-    <p style="width: 120px;"><a href="FF7%20les%20mods.php">ff7 mods</a><br>
-    </p>
-    <p style="width: 120px;"><a href="ff7modslist%20TEST.php">ff7 modlist</a><br>
-    </p>
-    <a href="ff7modslist%20TEST.php">ff7 modding</a><br>
-    <p style="width: 120px;"><span style="text-decoration: underline;"><span style="font-style: italic;"><span
-            style="font-weight: bold;"><br>
-          </span></span></span></p>
-    <p style="width: 120px;"><span style="text-decoration: underline;"><span style="font-style: italic;"><span
-            style="font-weight: bold;">INTERNET</span></span></span></p>
-    <p style="width: 120px;"><a href="Rez%20connexion%20ssh.php">connexions ssh</a><br>
-    </p>
-    <p style="width: 120px;"><a title="liens" href="internet%20les%20liens.php">quelques
-        liens sympa</a></p>
-    <p style="width: 120px;"><a title="vbub" href="file:///D:/dl/Base%20BSOD/Base%20BSOD/massinfo/virutalbox%20ubuntu%20.php"><br>
-      </a></p>
-    <p style="width: 120px;"><a title="vbub" href="virutalbox%20ubuntu%20.php">vbox
-        et buntu</a><br>
-    </p>
-    <p style="width: 120px;"><span style="font-weight: bold; font-style: italic; text-decoration: underline;"><br>
-      </span></p>
-    <p style="width: 120px;"><span style="font-weight: bold; font-style: italic; text-decoration: underline;">LINUX</span><br>
-    </p>
-    <a title="arborescence nux" href="linux%20arborescence.php">arborescence</a><br>
-    <br>
-    <a title="partoches" href="linux%20partition.php">partition</a><br>
-    <br>
-    <a href="linux%20les%20formats%20fichiers.php">type de fichiers</a><br>
-    <br>
-    <a href="nutyx.php">nutyx</a><br>
-    <br>
-    <a href="linux%20openbox.php">openbox</a><br>
-    <br>
-    <br>
-    Debian<br>
-    <br>
-    <a title="debian openbox" href="debian%20a%20la%20mano%20sid%20et%20openbox.php">debian
-      a la main</a><br>
-    <br>
-    <a title="debian sur usb" href="debian%20sur%20usb.php">debian sur usb</a><br>
-    <br>
-    <br>
-    <span style="font-weight: bold; font-style: italic; text-decoration: underline;">VIDEO</span><br>
-    <br>
-    <a title="les formats" href="les%20formats.php">les formats</a><br>
-    <br>
-    <a title="encodage x264" href="video%20encodage%20x264.php">encode h264</a><br>
-    <br>
-    sous titres<br>
-    <br>
-    sons<br>
-    <br>
-    tag<br>
-    <br>
-    <br>
-    <span style="font-weight: bold; font-style: italic; text-decoration: underline;">PERIPH</span><br>
-    <br>
-    <a title="les manettes" href="periph,%20les%20manettes.php">les manettes<br>
-    </a><br>
-    <a href="gps%20tuto%20install.php">GPS</a><br>
-    <br>
-    <span style="font-weight: bold; font-style: italic; text-decoration: underline;"><br>
-      OLIMEX</span><br>
-    <br>
-    olimex20<br>
-    <br>
-    <a href="olimex%20build%20de%20debian.php">build debian a20</a><br>
-    <br>
-    <a href="lime%20serveur.php">serveur debian</a><br>
-    <br>
-    <a title="avec7pouce" href="olimex%20a20micro%20et%207.php">install avec un
-      7"</a><br>
-    <br>
-    <br>
-    <br>
-    <span style="font-weight: bold; font-style: italic; text-decoration: underline;">RASPBERRY</span><br>
-    <br>
-    <a href="raspb%20infos.php">infos</a><br>
-    <br>
-    <a href="raspb%20installation.php">preinstall</a><br>
-    <br>
-    <a href="raspb%20installation.php">install</a><br>
-    <br>
-    <a href="raspb%20modding.php">modding</a><br>
-    <br>
-    <br>
-    <span style="font-weight: bold; font-style: italic; text-decoration: underline;">CONSOLES</span><br>
-    <br>
-    <a title="les trucs de la ps2" href="PS2%20les%20trucs.php">ps2</a><br>
-    <br>
-    <br>
-    <span style="font-weight: bold; font-style: italic; text-decoration: underline;">SECU</span><br>
-    <br>
-    <a href="secu%20comptes%20et%20mdp.php">gestion de log/mdp<br>
-    </a><br>
-    <br>
-    <span style="font-weight: bold; font-style: italic; text-decoration: underline;">TELEPHONIE</span><br>
-    <br>
-    <a href="tel%20et%20net%20affaiblisement%20calcul.php">calcul affaiblissemt</a><br>
-    <br>
-    <a href="tel%20et%20net%20decouverte%20reseau%20ft.php">decouverte reseau</a><br>
-    <br>
-    <a href="tel%20et%20net%20prise%20adsl.php">prise adsl</a><br>
-    <br>
-    <a href="tel%20net%20installation%20interieur%20complet.php">installation
-      interieur</a><br>
-    <br>
-    <a href="tel%20net%20resau%20interieur.php">reseau interieur</a><br>
-    <br>
-    <br>
-    <span style="font-weight: bold; font-style: italic; text-decoration: underline;">ELECTRONIQUE</span><br>
-    <br>
-    <a href="electronique%20programeur%20pic%20via%20serie.php">programmeur de
-      pic style JDM</a><br>
-    <br>
-    <a href="electronique%20le%20minijack.php">le minijack</a><br>
-    <br>
-    <br>
-    <span style="font-weight: bold; font-style: italic; text-decoration: underline;">DEPANNAGE<br>
-      <br>
-    </span>les bases<br>
-    <br>
-    ecran pc portable hs<br>
-    <br>
-    depannage portable<br>
-    <br>
-    note de survie<br>
-    <br>
-    <br>
-    win 7<br>
-    <br>
-  </body>
-</html>
+<?php
+  $dir = '.';
+  $files = scandir($dir);
+
+  $menu = array();
+  foreach ($files as &$file) {
+    $html = get_meta_tags($file, true);
+    $title = $html["title"];
+    $tag = $html["keywords"];
+    if ($title) {
+      if ($menu[$tag]) {
+        array_push($menu[$tag], $title, $file);
+      } else {
+        $menu[$tag] = array(
+          0 => $title,
+          1 => $file,
+        );
+      }
+    }
+  }
+
+  var_dump($menu);
+
+  echo '<a href="index.php">Index</a>';
+
+  foreach ($menu as $keyword => $titles) {
+    echo '<h2>' . $keyword . '</h2>';
+    echo '<br/>';
+    for ($i = 0; $i < count($titles); $i += 2) {
+      echo '<a href="' . $titles[$i + 1] . '">' . $titles[$i] . '</a>';
+      echo '<br/>';
+    }
+  }
+?>
